9948712a5b82ea1fb539bdfc6866455a3adeb5ee
Jose Angel <joseangel@MacBook-Pro-de-Jose.local>
2016-11-05 11:21:01 +0100

    se corrige la palabra sipnosis por sinopsis

--- js/GestionPeliculas.js
+++ js/GestionPeliculas.js
@@ -5,16 +5,16 @@ function initializeEvents(){
     daoAjax = new ClassDaoAjax();
 
     $( "#fecha" ).datepicker();
-    $("#guardar").click(function(){daoAjax.add({titulo:$("#titulo").val(),director:$("#director").val(),sipnosis:$("#sipnosis").val(),fecha:$("#fecha").val()},peticionCompletada,peticionFallida)});
+    $("#guardar").click(function(){daoAjax.add({titulo:$("#titulo").val(),director:$("#director").val(),sinopsis:$("#sinopsis").val(),fecha:$("#fecha").val()},peticionCompletada,peticionFallida)});
     $("#borrar").click(function(){daoAjax.delete(id,peticionCompletada,peticionFallida)});
-    $("#modificar").click(function(){daoAjax.update({titulo:$("#titulo").val(),director:$("#director").val(),sipnosis:$("#sipnosis").val(),fecha:$("#fecha").val()},id,peticionCompletada,peticionFallida)}); 
+    $("#modificar").click(function(){daoAjax.update({titulo:$("#titulo").val(),director:$("#director").val(),sinopsis:$("#sinopsis").val(),fecha:$("#fecha").val()},id,peticionCompletada,peticionFallida)}); 
     
     daoAjax.load(resultadoGet);
 }
 function peticionCompletada(data, status){
     $("#titulo").val("");
     $("#director").val("");
-    $("#sipnosis").val("");
+    $("#sinopsis").val("");
     $("#fecha").val("");
     daoAjax.load(resultadoGet);
 }
@@ -24,14 +24,14 @@ function peticionFallida(jqXHR, status, error){
 function resultadoGet(data,status){
     if(status == "success"){
         $("table").empty();
-        $("table").append("<thead><tr><td>ID</td><td>TITULO</td><td>DIRECTOR</td><td>SIPNOSIS</td><td>FECHA</td></tr></thead>");
+        $("table").append("<thead><tr><td>ID</td><td>TITULO</td><td>DIRECTOR</td><td>SINOPSIS</td><td>FECHA</td></tr></thead>");
         for(let peliculas = 0; peliculas < data.length;peliculas++){
             if(peliculas == 0){
               $("table").append("<thbody>")
             }else if(peliculas == (data-length - 1)){
                $("table").append("</thbody>")
             }
-            $("table").append("<tr><td>" + data[peliculas].id +"</td>"+"<td>"+data[peliculas].titulo+"</td>"+"<td>"+data[peliculas].director+"</td>"+"<td>"+data[peliculas].sipnosis+"</td>"+"<td>"+data[peliculas].fecha+"</td></tr>");
+            $("table").append("<tr><td>" + data[peliculas].id +"</td>"+"<td>"+data[peliculas].titulo+"</td>"+"<td>"+data[peliculas].director+"</td>"+"<td>"+data[peliculas].sinopsis+"</td>"+"<td>"+data[peliculas].fecha+"</td></tr>");
         }
         $("tr").click(presionFila);
     }
@@ -45,7 +45,7 @@ function presionFila(){
         id = arrayCeldas[0];
         $("#titulo").val(arrayCeldas[1]);
         $("#director").val(arrayCeldas[2]);
-        $("#sipnosis").val(arrayCeldas[3]);
+        $("#sinopsis").val(arrayCeldas[3]);
         $("#fecha").val(arrayCeldas[4]);
     }
 }
@@ -63,10 +63,10 @@ function validar(){
       cargarErrorValidacion("director");
       validacion = false;
   }
-  if($("#sipnosis").val() != ""){
-    limpiarErrorValidacion("sipnosis");
+  if($("#sinopsis").val() != ""){
+    limpiarErrorValidacion("sinopsis");
   }else{
-      cargarErrorValidacion("sipnosis");
+      cargarErrorValidacion("sinopsis");
       validacion = false;
   }
   if($("#fecha").val() != ""){
